db0a5d4376224aef0e03ee80633c38c0b3e9bf15
MarinaSeregina <marysiaseregina@gmail.com>
2020-02-26 12:02:44 +0200

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+
+$(function(){
+    $(".slider").bxSlider({
+    // minSlides: 3,
+    // maxSlides: 3,
+    // moveSlides: 1,
+    // slideWidth: 200,
+    // slideMargin: 10,
+    // auto: true
+    
+});
+});
+
+
+    $(function(){
+         $('.up').click(function () {
+    	 $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        });
+    });
---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// $(function(){
+// 	$(".slider").bxSlider({
+// 	// minSlides: 3,
+// 	// maxSlides: 3,
+// 	// moveSlides: 1,
+// 	// slideWidth: 200,
+// 	// slideMargin: 10,
+// 	// auto: true
+	
+// });
+// });
